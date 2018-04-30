04651ae2379d0ab349bffa1d65a6ba793e06a43e
Anthony Shaw <aajshaw@adsitsol.co.uk>
2018-04-30 18:25:05 +0100

    Added main web page and allow cross-site script access

--- server.js
+++ server.js
@@ -5,7 +5,12 @@ const app = express();
 
 const port = process.env.PORT || 8081
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 require('./routes.js')(app);
 
 app.listen(port);
-console.log("Listening on port " + port);
